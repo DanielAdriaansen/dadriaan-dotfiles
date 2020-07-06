f3293cc9161563bae3cca3065ccc752cddbdc2d0
Daniel Adriaansen <daniel.adriaansen@gmail.com>
2020-07-06 20:43:40 +0000

    Changes to install of vim directory.

--- setup.sh
+++ setup.sh
@@ -12,13 +12,18 @@ SCRIPT_DIR=$(dirname $(readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0))  #
 CURRENT_TIMESTAMP=$(date +%F_%T)
 
 # List of desired files
-my_files=("bashrc " "bash_profile" "bash_functions" "bash_aliases" "vimrc" "condarc" "vim")
+my_files=("bashrc " "bash_profile" "bash_functions" "bash_aliases" "vimrc" "condarc" "tmux.conf")
 
 # Install desired files using symlinks to the git repo
 # git repo location is stored as ${SCRIPT_DIR}
 for f in ${my_files[@]}; do
   install_dotfile ${f} ${CURRENT_TIMESTAMP} ${SCRIPT_DIR}
 done
+
+# Install vim directory
+[ ! -d ~/.vim ] || mv ~/.vim ~/.vim.${CURRENT_TIMESTAMP}
+check_return_code "Error saving ~/.vim to ~/.vim.${CURRENT_TIMESTAMP}"
+ln -s ${SCRIPT_DIR}/vim ~/.vim
 
 printf "\nSetup of ${HOME} completed succesfully\n"
 
