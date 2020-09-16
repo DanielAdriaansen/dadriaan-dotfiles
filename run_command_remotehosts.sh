378fdfdbf89a4efdeef8f57c2ae622f779ff9f1e
Daniel Adriaansen <daniel.adriaansen@gmail.com>
2020-09-15 20:37:00 -0600

    Script for running command on remotehost over SSH.

---
+++ run_command_remotehosts.sh
@@ -0,0 +1,18 @@
+CMD='cd ~/git/dadriaan-dotfiles; git remote set-url origin git@github.com:NCAR/dadriaan-dotfiles.git'
+#CMD='mv ~/git/dotfiles ~/git/dadriaan-dotfiles'
+#CMD='cd ~/git/dadriaan-dotfiles; git remote -v'
+
+function run_command() {
+
+  echo ssh -Y ${USER}@$1.rap.ucar.edu \'${CMD}\'
+  ssh -Y ${USER}@$1.rap.ucar.edu ${CMD}
+  
+}
+
+host_list=("cessna" "khaba" "radia" "nikara" "sonbef" "ahmose" "horus" "huni" "djoser" "dakota" "doohan" "etage" "eyewall" "floyd"\
+             "grebe" "kiowa" "mizu" "raneb" "tuvok" "content")
+
+for h in ${host_list[@]}; do
+  run_command ${h}
+done
+
